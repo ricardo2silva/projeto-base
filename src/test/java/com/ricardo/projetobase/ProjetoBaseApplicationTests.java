3e66fff55ebeeedd08d34a16386c38bd0319b0f2
ricardo2silva <ricardojonasdasilva60@gmail.com>
2023-11-27 23:08:51 -0300

    first commit

---
+++ src/test/java/com/ricardo/projetobase/ProjetoBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardo.projetobase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
